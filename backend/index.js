ed764150c4cea2d92d6be820abe6176a6f7d61a1
Aolith <3775001306@qq.com>
2026-04-20 14:49:07 +0800

    安装了nodemon依赖

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
 //通过express搭建Web服务器
-const express=require("express")
+const express=require('express')
 
 const app=express()
 const port=3001
 
 //用get请求访问路径
 app.get('/', (req, res) => {
-  res.send('Hello Express')
+  res.send('你好 Express!')
 })
-
+//用post请求访问路径/login
+app.post('/login',(req,res)=>{
+  res.send('登录成功')
+})
+//在指定端口启动服务器
 app.listen(port,()=>{
   console.log(`服务器在${port}端口启动成功`)
 })
