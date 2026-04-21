74b4cda2a7d55b26664d8eea4848ecc67e1b2d10
Aolith <3775001306@qq.com>
2026-04-21 08:50:17 +0800

    创建路由

--- backend/index.js
+++ backend/index.js
@@ -5,38 +5,42 @@ const path=require('path')
 const app=express()
 const port=3001
 
-// 1. 设置模板文件存放的目录，这里是指向项目根目录下的 'views' 文件夹
-app.set('views', path.join(__dirname, 'views'));
-// 2. 设置使用的模板引擎为 ejs
-//app.set('view engine', 'ejs')
-app.set('view engine','html')
-// 3. 设置指定后缀名的文件用什么引擎
-app.engine('html',require('ejs').__express)
+//这是一个路由
+app.get('/',(req,res)=>{
+  res.send('hi')
+})
 
-/* app.get('/user/:id',(req,res)=>{
-  const {id}= req.params
-  res.render('user.ejs',{
-    id
-  })
-}) */
+//用户登录
+app.post('/user/login',(req,res)=>{
+  res.send('登录成功')
+})
+//用户注册
+app.post('/user/register',(req,res)=>{
+  res.send('注册成功')
+})
 
 app.get('/user/:id',(req,res)=>{
-  const {id}= req.params
-
-  res.render('user',{
-    id,
-    title:`用户${id}的首页`,
-    html:'<h1>我是html字符串</h1>',
-    user:[{
-    username:'Alex',
-    gender:'male'
-    },
-    {
-    username:'Aim',
-    gender:'female'
-    }]
-  })
+  const {id} =req.params
+  res.send(`用户${id}完成登录`)
 })
+
+//发布动态
+app.post('/dynamic',(req,res)=>{
+  res.send('发布动态成功')
+})
+//获取动态
+app.get('/dynamic',(req,res)=>{
+  res.send('获取动态成功')
+})
+//更新动态
+app.put('/dynamic',(req,res)=>{
+  res.send('更新动态成功')
+})
+//删除动态
+app.delete('/dynamic',(req,res)=>{
+  res.send('删除动态成功')
+})
+
 
 //在指定端口启动服务器
 app.listen(port,()=>{
