9a63a27bca4a1862de491ff7a02a55a1d6dd45ac
Aolith <3775001306@qq.com>
2026-04-24 14:20:17 +0800

    数据源独立

--- backend/router/posts.js
+++ backend/router/posts.js
@@ -1,12 +1,8 @@
 const express=require('express')
 const postRouter=express.Router()
 
-//硬编码数据 
-const posts = [
-  { id: 1, title: '第一条帖子', content: '我终于要开始写论坛了！', likes: 0, comments: [] },
-  { id: 2, title: '第二条帖子', content: '有点激动呢！', likes: 0, comments: [] },
-  { id: 3, title: '第三条帖子', content: '我会加油！', likes: 0, comments: [] }
-  ]
+const posts = require('../data/post');//引入数据
+
 //get接口
 postRouter.get('/',(req,res)=>{
   res.json(posts)
