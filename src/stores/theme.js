bdf91d909eadb0a0aeee468dd9e6f7ddaecb485a
Aolith <3775001306@qq.com>
2026-04-24 22:37:10 +0800

    优化一下theme2

--- src/stores/theme.js
+++ src/stores/theme.js
@@ -1,4 +1,4 @@
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { defineStore } from "pinia";
 
 export const useThemeStore=defineStore('theme',()=>{
@@ -7,6 +7,10 @@ export const useThemeStore=defineStore('theme',()=>{
   const saved = localStorage.getItem(STORAGE_KEY)//本地持久化
 
   const theme = ref(saved || 'light')
+//添加watch
+watch(theme,(newtheme)=>{
+   localStorage.setItem(STORAGE_KEY, newtheme)
+})
 
   function toggleTheme() {
     theme.value = theme.value === 'light' ? 'dark' : 'light'
