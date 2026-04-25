7afc5493715deba06e01b6f5fa6d5f108688e32b
Aolith <3775001306@qq.com>
2026-04-25 22:55:05 +0800

    移除 data/ 文件夹和临时迁移脚本，完成全栈数据库迁移

--- backend/data/post.js
+++
@@ -1,8 +0,0 @@
-//硬编码数据 
-const posts = [
-  { id: 1, title: '第一条帖子', content: '我终于要开始写论坛了！', likes: 0, comments: [] },
-  { id: 2, title: '第二条帖子', content: '有点激动呢！', likes: 0, comments: [] },
-  { id: 3, title: '第三条帖子', content: '我会加油！', likes: 0, comments: [] }
-  ]
-
-module.exports=posts
